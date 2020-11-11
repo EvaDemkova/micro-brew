63400542308c017b1e097b462277dc4ee8877013
Aurelien Gaillard <fakeemail@gadzo.org>
2020-11-11 20:05:55 +0100

    styling beerpost

--- frontend/src/MainApp/components/Beerpost.js
+++ frontend/src/MainApp/components/Beerpost.js
@@ -1,21 +1,50 @@
 import React from 'react'
+import { IoMdBeer } from 'react-icons/io'
+import './beerpost.scss'
 
 const Beerpost = () => {
   return (
     <div className='beerpost'>
       <div className='beerpost__preview'>
         <div className='left-menu'>
-          <div className='user-info'>photo</div>
+          <div className='user-info'>
+            <img
+              src='https://vignette.wikia.nocookie.net/heros/images/4/42/Alice_Disney_Infobox.jpg/revision/latest/scale-to-width-down/310?cb=20200622124942&path-prefix=fr'
+              alt=''
+            />
+            <p>Alice and Bob</p>
+          </div>
           <div className='updated-time'>Posted : 3 hours ago</div>
         </div>
         <div className='preview-info'>
-          <div className='beerpost__header'>
-            <div className='logo'>X</div>
+          <div className='header'>
+            <div className='logo'>
+              <IoMdBeer />
+            </div>
             <div className='title'>
               <h3>My great beer</h3>
               <p>I made this nice Lager with love</p>
             </div>
           </div>
+          <div className='status'>Status Fermentation</div>
+          <div className='type'>Type - Lager</div>
+          <div className='beerpost__stats'>
+            <ul>
+              <li>ABV</li>
+              <li>OG</li>
+              <li>EBC</li>
+              <li>IBU</li>
+              <li>Carbonation</li>
+              <li>Gravity</li>
+              <li>Batch Volume</li>
+            </ul>
+          </div>
+        </div>
+        <div className='beerpost__photos'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/d/da/Pilsner_urquell_mug.jpg'
+            alt=''
+          />
         </div>
       </div>
     </div>

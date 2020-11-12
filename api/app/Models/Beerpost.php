b44eca69a25391d38944457732866d9eb08b2eb2
Aurelien Gaillard <fakeemail@gadzo.org>
2020-11-12 16:38:19 +0100

    relationshiips

--- api/app/Models/Beerpost.php
+++ api/app/Models/Beerpost.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Beerpost_ingredient;
+use App\Models\Ingredient;
 
 class Beerpost extends Model
 {
     use HasFactory;
+
+    public function ingredients() 
+    {
+        return $this->belongsToMany(Ingredient::class, 'beerpost_ingredients')->withPivot('ingredient_name','quantity');
+    }
 }
--- api/routes/web.php
+++ api/routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/api/beerposts/users/{id}', 'BeerpostController@show_by_user_id');

74c676ca419b3e4a1ffd606b2c33d8ebacea7e4f
Aurelien Gaillard <fakeemail@gadzo.org>
2020-11-16 22:47:19 +0100

    list follow updated

--- api/resources/js/frontend/MainApp/components/ListFollow.js
+++ api/resources/js/frontend/MainApp/components/ListFollow.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/listFollow.scss";
 
 const ListFollow = () => {
+    const [followList, setFollowList] = useState([]);
+
+    const fetchFollowList = async () => {
+        const response = await fetch(`/api/users/follow_list_proposal`);
+        const data = await response.json();
+        setFollowList(data);
+    };
+    console.log(followList);
+    useEffect(() => {
+        fetchFollowList();
+    }, []);
+
     return (
         <div className="list-follow">
             <h2>Who to follow</h2>
-            <div className="user-card">
-                <div className="user-card__infos">Joel</div>
-                <div className="button-list">
-                    <button>View Profile</button>
-                    <button>Follow</button>
-                </div>
-            </div>
+            {followList.map(user => {
+                return (
+                    <div className="user-card">
+                        <div className="user-card__infos">{user.name}</div>
+                        <div className="button-list">
+                            <button>View Profile</button>
+                            <button>Follow</button>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 };

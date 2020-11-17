7c5b5d645cffee363c62f035be4d5fcbb4ebccd1
Aurelien Gaillard <fakeemail@gadzo.org>
2020-11-17 16:30:53 +0100

    change router

--- api/resources/js/frontend/App.js
+++ api/resources/js/frontend/App.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Homepage from './Homepage/Homepage'
-import MainApp from './MainApp'
+import Homepage from "./Homepage/Homepage";
+import MainApp from "./MainApp";
+import Dashboard from "./MainApp/Dashboard";
+import Feed from "./MainApp/Feed";
+import Map from "./MainApp/Map";
+import Profile from "./MainApp/Profile";
+import Header from "./MainApp/Header";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Homepage />
-        </Route>
-        <Route path='*'>
-          <MainApp />
-        </Route>
-      </Switch>
-    </Router>
-  )
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+                <Route path="/dashboard/:id">
+                    <Header />
+                    <Dashboard />
+                </Route>
+                <Route path="/feed">
+                    <Header />
+                    <Feed />
+                </Route>
+                <Route path="/map">
+                    <Header />
+                    <Map />
+                </Route>
+                <Route path="/profile">
+                    <Header />
+                    <Profile />
+                </Route>
+                {/* <Route path="*">
+                  <MainApp />
+              </Route> */}
+            </Switch>
+        </Router>
+    );
 }
 
-export default App
+export default App;

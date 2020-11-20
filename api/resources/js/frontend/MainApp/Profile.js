83cb930629f9e260b45eaf17decab9ab6b0dd49e
Aurelien Gaillard <fakeemail@gadzo.org>
2020-11-20 10:29:12 +0100

    profile form default value

--- api/resources/js/frontend/MainApp/Profile.js
+++ api/resources/js/frontend/MainApp/Profile.js
@@ -1,169 +1,214 @@
-import React, { useState, useEffect } from 'react';
-import './styles/profile.scss';
+import React, { useState, useEffect } from "react";
+import "./styles/profile.scss";
 import { MdEdit } from "react-icons/md";
-import Dropzone from './components/BeerpostForm/Dropzone';
-import Uploader from './components/Uploader';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import SaveBtn from './components/SaveBtn';
-import axios from 'axios';
+import Dropzone from "./components/BeerpostForm/Dropzone";
+import Uploader from "./components/Uploader";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import SaveBtn from "./components/SaveBtn";
+import axios from "axios";
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
+const useStyles = makeStyles(theme => ({
+    root: {
+        "& .MuiTextField-root": {
+            margin: theme.spacing(1),
+            width: "25ch"
+        }
+    }
 }));
 
 const Profile = () => {
-  const [file, setFile] = useState([]);
-  const [user, setUser] = useState({
-    name: "", 
-    email: "", 
-    street: "", 
-    city: "", 
-    country: "", 
-    photo: "", 
-    equipment: ""
-  })
-  const [edit, setEdit] = useState(false);
-  const classes = useStyles();
+    const [file, setFile] = useState([]);
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        street: "",
+        city: "",
+        country: "",
+        photo: "",
+        equipment: ""
+    });
+    const [edit, setEdit] = useState(false);
+    const classes = useStyles();
 
-  const fetchDatas = async () => {
-    const response = await fetch(`/api/profile`);
-    const data = await response.json();
-    setUser((prev) => ({
-      ...prev,
-      name: data.user.name,
-      email: data.user.email,
-      street: data.user.street,
-      city: data.user.city,
-      country: data.user.country,
-      photo: ((data.user.profile_photo)? (data.user.profile_photo) : '/uploads/profile-photos/user.png'),
-      equipment: ((data.equipment.legth === 0) ? (data.equipment[0].name) : "NA" )
-    }))
-    setFile([data.user.profile_photo]);
-    console.log(data.equipment);
-  };
-  
-  useEffect(() => {
-    fetchDatas();
-  }, [])
+    const fetchDatas = async () => {
+        const response = await fetch(`/api/profile`);
+        const data = await response.json();
+        setUser(prev => ({
+            ...prev,
+            name: data.user.name,
+            email: data.user.email,
+            street: data.user.street || "",
+            city: data.user.city || "",
+            country: data.user.country || "",
+            photo: data.user.profile_photo
+                ? data.user.profile_photo
+                : "/uploads/profile-photos/user.png",
+            equipment:
+                data.equipment.legth === 0 ? data.equipment[0].name : "NA"
+        }));
+        setFile([data.user.profile_photo]);
+        console.log(data.equipment);
+    };
 
-  const handleSubmit = async (e) => { 
-    console.log(file)
-    console.log(user.equipment);
-    e.preventDefault();
-    await axios.get("/sanctum/csrf-cookie");
-    await axios
-      .post('/api/users/update', user)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-    setEdit(false)
-  }
+    useEffect(() => {
+        fetchDatas();
+    }, []);
 
-  if (edit) {
-    return (
-      <main>
-        <form className={classes.root} noValidate autoComplete="off">
-        <Uploader file={file} setFile={setFile} image={user.photo}/>
-          <div className="prof-card__row">
-            <TextField
-              id="outlined-helperText"
-              label="Name"
-              value={user.name}
-              variant="outlined"
-              onChange={(e) => setUser((prev) => ({ ...prev, name: e.target.value }))} 
-            />
-        </div>
-        <div className="prof-card__row">
-           <TextField
-          id="outlined-helperText"
-          label="Email"
-          variant="outlined"
-          value={user.email}
-          onChange={(e) => setUser((prev) => ({ ...prev, email: e.target.value }))} 
-          />
-          </div>
-          <div className="prof-card__row">
-           <TextField
-          id="outlined-helperText"
-          label="Street"
-          value={user.street}
-          variant="outlined"
-          onChange={(e) => setUser((prev) => ({ ...prev, street: e.target.value }))} 
-          />
-          </div>
-          <div className="prof-card__row">
-           <TextField
-          id="outlined-helperText"
-          label="City"
-          value={user.city}
-          variant="outlined"
-          onChange={(e) => setUser((prev) => ({ ...prev, city: e.target.value }))} 
-          />
-          </div>
-          <div className="prof-card__row">
-           <TextField
-          id="outlined-helperText"
-          label="Country"
-          value={user.country}
-          variant="outlined"
-          onChange={(e) => setUser((prev) => ({ ...prev, country: e.target.value }))} 
-          />
-          </div>
-          <div className="prof-card__row">
-           <TextField
-          id="outlined-helperText"
-          label="Equipment"
-          value={user.equipment}
-          variant="outlined"
-          onChange={(e) => setUser((prev) => ({ ...prev, equipment: e.target.value }))} 
-          />
-          </div>
-          <SaveBtn handleSubmit={handleSubmit }/>
-        </form>
-      </main>
-    )
-  } else {
-    return (
-      <main className="prof-card">
-        <img className="prof-card__picture" src={(user.photo)? (user.photo): '/uploads/profile-photos/user.png'} alt={user.name} />
-        {/* here comes dropzone or input for file */}
-          <h2>{ user.name}</h2>
-        <div className="prof-card__row">
-          <h3>Email:</h3>
-          <p>{ user.email}</p>
-        </div>
-        <div className="prof-car__row">
-          <h3>Address:</h3>
-        {
-          (( user.street  && user.city && user.country) === null)
-          ?
-          <p>NA</p>
-          :
-          <p>
-            {user.street}<br />
-            {user.city}<br />
-            {user.country}<br/>
-            </p>   
-        }
-        </div>
-        <div className="prof-card__row">
-          <h3>Eqipment:</h3>
-          <p>{user.equipment}</p>
-        </div>
-        <MdEdit className="edit-icon" onClick={()=>setEdit(true)}/>
-      </main>
-    )
-  }
-}
+    const handleSubmit = async e => {
+        console.log(file);
+        console.log(user.equipment);
+        e.preventDefault();
+        await axios.get("/sanctum/csrf-cookie");
+        await axios
+            .post("/api/users/update", user)
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        setEdit(false);
+    };
 
-export default Profile
+    if (edit) {
+        return (
+            <main>
+                <form className={classes.root} noValidate autoComplete="off">
+                    <Uploader
+                        file={file}
+                        setFile={setFile}
+                        image={user.photo}
+                    />
+                    <div className="prof-card__row">
+                        <TextField
+                            id="outlined-helperText"
+                            label="Name"
+                            value={user.name}
+                            variant="outlined"
+                            onChange={e =>
+                                setUser(prev => ({
+                                    ...prev,
+                                    name: e.target.value
+                                }))
+                            }
+                        />
+                    </div>
+                    <div className="prof-card__row">
+                        <TextField
+                            id="outlined-helperText"
+                            label="Email"
+                            variant="outlined"
+                            value={user.email}
+                            onChange={e =>
+                                setUser(prev => ({
+                                    ...prev,
+                                    email: e.target.value
+                                }))
+                            }
+                        />
+                    </div>
+                    <div className="prof-card__row">
+                        <TextField
+                            id="outlined-helperText"
+                            label="Street"
+                            value={user.street}
+                            variant="outlined"
+                            onChange={e =>
+                                setUser(prev => ({
+                                    ...prev,
+                                    street: e.target.value
+                                }))
+                            }
+                        />
+                    </div>
+                    <div className="prof-card__row">
+                        <TextField
+                            id="outlined-helperText"
+                            label="City"
+                            value={user.city}
+                            variant="outlined"
+                            onChange={e =>
+                                setUser(prev => ({
+                                    ...prev,
+                                    city: e.target.value
+                                }))
+                            }
+                        />
+                    </div>
+                    <div className="prof-card__row">
+                        <TextField
+                            id="outlined-helperText"
+                            label="Country"
+                            value={user.country}
+                            variant="outlined"
+                            onChange={e =>
+                                setUser(prev => ({
+                                    ...prev,
+                                    country: e.target.value
+                                }))
+                            }
+                        />
+                    </div>
+                    <div className="prof-card__row">
+                        <TextField
+                            id="outlined-helperText"
+                            label="Equipment"
+                            value={user.equipment}
+                            variant="outlined"
+                            onChange={e =>
+                                setUser(prev => ({
+                                    ...prev,
+                                    equipment: e.target.value
+                                }))
+                            }
+                        />
+                    </div>
+                    <SaveBtn handleSubmit={handleSubmit} />
+                </form>
+            </main>
+        );
+    } else {
+        return (
+            <main className="prof-card">
+                <img
+                    className="prof-card__picture"
+                    src={
+                        user.photo
+                            ? user.photo
+                            : "/uploads/profile-photos/user.png"
+                    }
+                    alt={user.name}
+                />
+                {/* here comes dropzone or input for file */}
+                <h2>{user.name}</h2>
+                <div className="prof-card__row">
+                    <h3>Email:</h3>
+                    <p>{user.email}</p>
+                </div>
+                <div className="prof-car__row">
+                    <h3>Address:</h3>
+                    {(user.street && user.city && user.country) === null ? (
+                        <p>NA</p>
+                    ) : (
+                        <p>
+                            {user.street}
+                            <br />
+                            {user.city}
+                            <br />
+                            {user.country}
+                            <br />
+                        </p>
+                    )}
+                </div>
+                <div className="prof-card__row">
+                    <h3>Eqipment:</h3>
+                    <p>{user.equipment}</p>
+                </div>
+                <MdEdit className="edit-icon" onClick={() => setEdit(true)} />
+            </main>
+        );
+    }
+};
+
+export default Profile;

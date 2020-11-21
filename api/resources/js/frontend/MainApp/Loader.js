9ae6f2be3dac02fb48194c8c790b80cdd3c8f6cc
Aurelien Gaillard <fakeemail@gadzo.org>
2020-11-21 23:20:25 +0100

    Loader

--- api/resources/js/frontend/MainApp/Loader.js
+++ api/resources/js/frontend/MainApp/Loader.js
@@ -1,15 +1,25 @@
+import { Autocomplete } from "@material-ui/lab";
 import React from "react";
 import Loader from "react-loader-spinner";
 
+const style = {
+    width: "100vw",
+    height: "50vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+};
 const LoaderComp = () => {
     return (
-        <Loader
-            type="Circles"
-            color="#00BFFF"
-            height={100}
-            width={100}
-            timeout={3000} //3 secs
-        />
+        <div style={style}>
+            <Loader
+                type="Circles"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                timeout={10000} //10 secs
+            />
+        </div>
     );
 };
 

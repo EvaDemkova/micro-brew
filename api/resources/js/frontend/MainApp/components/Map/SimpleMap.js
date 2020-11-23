315f222df5c914ebb115ddf12221cfdce520b0a2
Aurelien Gaillard <fakeemail@gadzo.org>
2020-11-23 15:26:23 +0100

    map without label

--- api/resources/js/frontend/MainApp/components/Map/SimpleMap.js
+++ api/resources/js/frontend/MainApp/components/Map/SimpleMap.js
@@ -8,166 +8,166 @@ import Loader from "../Loader";
 import "./simpleMap.scss";
 import { Button, TextField } from "@material-ui/core";
 
-// const exampleMapStyles = [
-//     {
-//         elementType: "geometry",
-//         stylers: [
-//             {
-//                 color: "#f5f5f5"
-//             }
-//         ]
-//     },
-//     {
-//         elementType: "labels.icon",
-//         stylers: [
-//             {
-//                 visibility: "off"
-//             }
-//         ]
-//     },
-//     {
-//         elementType: "labels.text.fill",
-//         stylers: [
-//             {
-//                 color: "#616161"
-//             }
-//         ]
-//     },
-//     {
-//         elementType: "labels.text.stroke",
-//         stylers: [
-//             {
-//                 color: "#f5f5f5"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "administrative.land_parcel",
-//         elementType: "labels.text.fill",
-//         stylers: [
-//             {
-//                 color: "#bdbdbd"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "poi",
-//         elementType: "geometry",
-//         stylers: [
-//             {
-//                 color: "#eeeeee"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "poi",
-//         elementType: "labels.text.fill",
-//         stylers: [
-//             {
-//                 color: "#757575"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "poi.park",
-//         elementType: "geometry",
-//         stylers: [
-//             {
-//                 color: "#e5e5e5"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "poi.park",
-//         elementType: "labels.text.fill",
-//         stylers: [
-//             {
-//                 color: "#9e9e9e"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "road",
-//         elementType: "geometry",
-//         stylers: [
-//             {
-//                 color: "#ffffff"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "road.arterial",
-//         elementType: "labels.text.fill",
-//         stylers: [
-//             {
-//                 color: "#757575"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "road.highway",
-//         elementType: "geometry",
-//         stylers: [
-//             {
-//                 color: "#dadada"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "road.highway",
-//         elementType: "labels.text.fill",
-//         stylers: [
-//             {
-//                 color: "#616161"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "road.local",
-//         elementType: "labels.text.fill",
-//         stylers: [
-//             {
-//                 color: "#9e9e9e"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "transit.line",
-//         elementType: "geometry",
-//         stylers: [
-//             {
-//                 color: "#e5e5e5"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "transit.station",
-//         elementType: "geometry",
-//         stylers: [
-//             {
-//                 color: "#eeeeee"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "water",
-//         elementType: "geometry",
-//         stylers: [
-//             {
-//                 color: "#c9c9c9"
-//             }
-//         ]
-//     },
-//     {
-//         featureType: "water",
-//         elementType: "labels.text.fill",
-//         stylers: [
-//             {
-//                 color: "#9e9e9e"
-//             }
-//         ]
-//     }
-// ];
+const exampleMapStyles = [
+    //     {
+    //         elementType: "geometry",
+    //         stylers: [
+    //             {
+    //                 color: "#f5f5f5"
+    //             }
+    //         ]
+    //     },
+    {
+        elementType: "labels.icon",
+        stylers: [
+            {
+                visibility: "off"
+            }
+        ]
+    }
+    //     {
+    //         elementType: "labels.text.fill",
+    //         stylers: [
+    //             {
+    //                 color: "#616161"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         elementType: "labels.text.stroke",
+    //         stylers: [
+    //             {
+    //                 color: "#f5f5f5"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "administrative.land_parcel",
+    //         elementType: "labels.text.fill",
+    //         stylers: [
+    //             {
+    //                 color: "#bdbdbd"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "poi",
+    //         elementType: "geometry",
+    //         stylers: [
+    //             {
+    //                 color: "#eeeeee"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "poi",
+    //         elementType: "labels.text.fill",
+    //         stylers: [
+    //             {
+    //                 color: "#757575"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "poi.park",
+    //         elementType: "geometry",
+    //         stylers: [
+    //             {
+    //                 color: "#e5e5e5"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "poi.park",
+    //         elementType: "labels.text.fill",
+    //         stylers: [
+    //             {
+    //                 color: "#9e9e9e"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "road",
+    //         elementType: "geometry",
+    //         stylers: [
+    //             {
+    //                 color: "#ffffff"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "road.arterial",
+    //         elementType: "labels.text.fill",
+    //         stylers: [
+    //             {
+    //                 color: "#757575"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "road.highway",
+    //         elementType: "geometry",
+    //         stylers: [
+    //             {
+    //                 color: "#dadada"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "road.highway",
+    //         elementType: "labels.text.fill",
+    //         stylers: [
+    //             {
+    //                 color: "#616161"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "road.local",
+    //         elementType: "labels.text.fill",
+    //         stylers: [
+    //             {
+    //                 color: "#9e9e9e"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "transit.line",
+    //         elementType: "geometry",
+    //         stylers: [
+    //             {
+    //                 color: "#e5e5e5"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "transit.station",
+    //         elementType: "geometry",
+    //         stylers: [
+    //             {
+    //                 color: "#eeeeee"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "water",
+    //         elementType: "geometry",
+    //         stylers: [
+    //             {
+    //                 color: "#c9c9c9"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         featureType: "water",
+    //         elementType: "labels.text.fill",
+    //         stylers: [
+    //             {
+    //                 color: "#9e9e9e"
+    //             }
+    //         ]
+    //     }
+];
 
 const SimpleMap = () => {
     const [center, setCenter] = useState({ lat: 50.073658, lng: 14.41854 });
@@ -265,9 +265,9 @@ const SimpleMap = () => {
                         bootstrapURLKeys={{
                             key: process.env.MIX_GOOGLE_API_KEY
                         }}
-                        // options={{
-                        //     styles: exampleMapStyles
-                        // }}
+                        options={{
+                            styles: exampleMapStyles
+                        }}
                         center={center}
                         defaultZoom={zoom}
                     >

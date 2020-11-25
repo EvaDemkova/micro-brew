08a7f474689c0c8578968999c4dfd8927840ba0d
Eva Demkova <evaademkova@gmail.com>
2020-11-25 11:59:27 +0100

    FTP deployment

--- api/resources/js/frontend/MainApp/components/Header/index.js
+++ api/resources/js/frontend/MainApp/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Logout from "./Logout";
+import useWindowWidth from "../useWindowWidth";
 
 import "./header.scss";
 import { useGlobalContext } from "../../../context";
@@ -8,14 +9,18 @@ import { useGlobalContext } from "../../../context";
 const Header = () => {
     const { user } = useGlobalContext();
     const { pathname } = useLocation();
+    const width = useWindowWidth();
 
-    return (
-        <header>
+    console.log(width)
+
+    if (width <= 700) {
+        return (
+            <header>
             <div className="logo">
                 <img src="/logo/toast.svg" alt="" />
                 <h3>Micro Brew</h3>
             </div>
-            <nav>
+            {/* <nav>
                 <ul>
                     <li className={/dashboard/.test(pathname) ? "active" : ""}>
                         <Link to={`/dashboard/${user.id}`}>Dashboard</Link>
@@ -30,11 +35,40 @@ const Header = () => {
                         <Link to="/profile">Profile</Link>
                     </li>
                 </ul>
-            </nav>
+            </nav> */}
 
             <Logout />
         </header>
-    );
+        )
+    } else {
+        return (
+            <header>
+                <div className="logo">
+                    <img src="/logo/toast.svg" alt="" />
+                    <h3>Micro Brew</h3>
+                </div>
+                <nav>
+                    <ul>
+                        <li className={/dashboard/.test(pathname) ? "active" : ""}>
+                            <Link to={`/dashboard/${user.id}`}>Dashboard</Link>
+                        </li>
+                        <li className={pathname == "/feed" ? "active" : ""}>
+                            <Link to="/feed">Feed</Link>
+                        </li>
+                        <li className={pathname == "/map" ? "active" : ""}>
+                            <Link to="/map">Map</Link>
+                        </li>
+                        <li className={pathname == "/profile" ? "active" : ""}>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                    </ul>
+                </nav>
+    
+                <Logout />
+            </header>
+        );
+    }
+
 };
 
 export default Header;

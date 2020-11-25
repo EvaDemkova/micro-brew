ef6d86b51c37aad7cc51815c9904e70bb871d584
Eva Demkova <evaademkova@gmail.com>
2020-11-25 09:51:22 +0100

    Welcome page styling

--- api/resources/js/frontend/MainApp/components/NewUserPage.js
+++ api/resources/js/frontend/MainApp/components/NewUserPage.js
@@ -1,21 +1,31 @@
 import React from "react";
+import './styles/newUserPage.scss';
 
 const NewUserPage = () => {
-    return (
-        <div className="newUserPage">
-            <h1>Hello, welcome in the MicroBrew community !</h1>
 
-            <p>Few tips to make your profile more active:</p>
-            <p>
-                Follow some brewers, we propose you a list of them on the right
-            </p>
-            <p>You can also find more brewers on the Map</p>
-            <p>
-                Update your profile to add a profile picture and your location
-            </p>
-            <p>Enter your first receipe in the Dashboard</p>
-        </div>
-    );
+    if (window.location.pathname === '/feed') {
+        return (
+            <div className="newUserPage">
+                <h1>Welcome to the <span>MicroBrew</span> community !</h1>
+
+                <h4>Few tips to make your profile more active:</h4>
+                <p>
+                    <span>Follow</span> some brewers, we propose you a list of them on the right.
+                </p>
+                <p>You can also find more brewers on the <span>Map</span></p>
+                <p>
+                    <span>Update your profile</span> to add a profile picture and your location
+                </p>
+                <p>Create your first recipe in your <span>Dashboard</span></p>
+            </div>
+        )
+    } else {
+        return (
+            <div className="newUserPage">
+                <h2>Create your first <span>Beerpost</span></h2>
+            </div>
+        )
+    }
 };
 
 export default NewUserPage;

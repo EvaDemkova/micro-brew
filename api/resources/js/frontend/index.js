321845e8d81f12669424bac0bab208098637ef3b
Aurelien Gaillard <fakeemail@gadzo.org>
2020-11-26 16:27:16 +0100

    strict mode removed

--- api/resources/js/frontend/index.js
+++ api/resources/js/frontend/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { AppProvider } from './context'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AppProvider } from "./context";
 
 ReactDOM.render(
-  <React.StrictMode>
+    // <React.StrictMode>
     <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+        <App />
+    </AppProvider>,
+    // </React.StrictMode>,
+    document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
